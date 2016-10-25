de90d9a81930fbbff1e742b4a18c2a47311384b8
Rickard Kamel <rickardkamel@gmail.com>
2016-10-25 23:53:09 +0200

    Created DataContext.

---
+++ DataService/DataContext.cs
@@ -0,0 +1,25 @@
+using Models;
+using System.Data.Entity;
+
+namespace DataService
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("RickardKamel")
+        {
+
+        }
+
+        public DbSet<About> About { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Resume> Resumes { get; set; }
+        public DbSet<TimeStamp> Timestamps { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
