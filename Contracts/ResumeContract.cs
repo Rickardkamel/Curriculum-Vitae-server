1ef75f4e2983770466674c6bb72cf985707a7a10
Rickard Kamel <rickardkamel@gmail.com>
2016-10-25 23:51:44 +0200

    Created contract for Resume.

---
+++ Contracts/ResumeContract.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class ResumeContract
+    {
+        public int Id { get; set; }
+        public string Section { get; set; }
+        public IList<TimeStampContract> TimeStamps { get; set; }
+    }
+}
