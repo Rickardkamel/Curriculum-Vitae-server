79df1a65c89486510ebc396888f0c75ce3d80ee2
Rickard Kamel <rickardkamel@gmail.com>
2016-10-25 23:50:51 +0200

    Created handler for Timestamps in Resume.

---
+++ BusinessLogic/Handlers/TimeStampHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using DataService;
+using DataService.UnitOfWork;
+using Mappers;
+using Models;
+
+namespace BusinessLogic.Handlers
+{
+    public class TimeStampHandler
+    {
+        private readonly UnitOfWork _uow;
+
+        public TimeStampHandler(object dataContext)
+        {
+            _uow = new UnitOfWork((DataContext)dataContext);
+        }
+
+        public List<TimeStampContract> Get()
+        {
+            return _uow.TimeStampRepository.GetAll().ToContracts();
+        }
+
+        public TimeStampContract Get(int id)
+        {
+            return _uow.TimeStampRepository.Get(id).ToContract();
+        }
+
+        public void Post(TimeStampContract timeStampContract)
+        {
+            _uow.TimeStampRepository.CreateOrUpdate(timeStampContract.ToEntity());
+        }
+
+        public void Delete(int id)
+        {
+            _uow.TimeStampRepository.Delete(id);
+        }
+    }
+}
