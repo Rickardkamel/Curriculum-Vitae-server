e70ea8d6e3e94cbe901b207068ce306dfc591869
Rickard Kamel <rickardkamel@gmail.com>
2016-10-25 23:52:42 +0200

    Created Migrations.

---
+++ DataService/Migrations/201610251653167_First.cs
@@ -0,0 +1,81 @@
+namespace DataService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class First : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Abouts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        BirthDate = c.DateTime(nullable: false),
+                        Adress = c.String(),
+                        Nationality = c.String(),
+                        Phone = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Projects",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Text = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Resumes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Section = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TimeStamps",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        SubTitle = c.String(),
+                        Text = c.String(),
+                        Date = c.DateTime(nullable: false),
+                        ResumeId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Resumes", t => t.ResumeId, cascadeDelete: false)
+                .Index(t => t.ResumeId);
+            
+            CreateTable(
+                "dbo.Skills",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Percentage = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TimeStamps", "ResumeId", "dbo.Resumes");
+            DropIndex("dbo.TimeStamps", new[] { "ResumeId" });
+            DropTable("dbo.Skills");
+            DropTable("dbo.TimeStamps");
+            DropTable("dbo.Resumes");
+            DropTable("dbo.Projects");
+            DropTable("dbo.Abouts");
+        }
+    }
+}
