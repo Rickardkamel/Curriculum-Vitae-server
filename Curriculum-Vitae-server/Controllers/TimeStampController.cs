8f52c6b1ea9f91185c01c79f8e99cdb9f5530c4a
Rickard Kamel <rickardkamel@gmail.com>
2016-10-26 19:01:07 +0200

    Created TimestampController.

---
+++ Curriculum-Vitae-server/Controllers/TimeStampController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusinessLogic.Handlers;
+using Contracts;
+using DataService;
+
+namespace Curriculum_Vitae_server.Controllers
+{
+    public class TimeStampController : ApiController
+    {
+        private readonly TimeStampHandler _timeStampHandler;
+
+        public TimeStampController()
+        {
+            _timeStampHandler = new TimeStampHandler(new DataContext());
+        }
+
+        public IEnumerable<TimeStampContract> Get()
+        {
+            return _timeStampHandler.Get();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var timeStamp = _timeStampHandler.Get(id);
+            return timeStamp == null ? (IHttpActionResult)NotFound() : Ok(timeStamp);
+        }
+
+        public IHttpActionResult Post(TimeStampContract timeStampContract)
+        {
+            _timeStampHandler.Post(timeStampContract);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            _timeStampHandler.Delete(id);
+            return Ok();
+        }
+    }
+}
