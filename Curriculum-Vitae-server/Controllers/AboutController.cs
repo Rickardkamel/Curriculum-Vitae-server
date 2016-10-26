c44577c4829f262c0ab2eeb93dcfdc49f5c8a911
Rickard Kamel <rickardkamel@gmail.com>
2016-10-26 19:00:35 +0200

    Created AboutController.

---
+++ Curriculum-Vitae-server/Controllers/AboutController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusinessLogic.Handlers;
+using Contracts;
+using DataService;
+
+namespace Curriculum_Vitae_server.Controllers
+{
+    public class AboutController : ApiController
+    {
+        private readonly AboutHandler _aboutHandler;
+
+        public AboutController()
+        {
+            _aboutHandler = new AboutHandler(new DataContext());
+        }
+
+        public IEnumerable<AboutContract> Get()
+        {
+            return _aboutHandler.Get();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var aboutMe = _aboutHandler.Get(id);
+            return aboutMe == null ? (IHttpActionResult)NotFound() : Ok(aboutMe);
+        }
+
+        public IHttpActionResult Post(AboutContract aboutContract)
+        {
+            _aboutHandler.Post(aboutContract);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            _aboutHandler.Delete(id);
+            return Ok();
+        }
+    }
+}
