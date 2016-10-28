65205266818d8509f883b8299c5943b0a55f09f7
rickardkamel <rickardkamel@gmail.com>
2016-10-28 09:56:36 +0200

    added From/To date to TimestampContract.

--- Contracts/TimeStampContract.cs
+++ Contracts/TimeStampContract.cs
@@ -8,7 +8,8 @@ namespace Contracts
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string Text { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
         public int ResumeId { get; set; }
     }
 }
