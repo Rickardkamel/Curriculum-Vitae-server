ff976daa76fbdc5c838170406fff237f33141927
Rickard Kamel <rickardkamel@gmail.com>
2016-11-09 18:00:28 +0100

    Added ReferenceModel.

---
+++ Models/Reference.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public class Reference
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Occupation { get; set; }
+        public string Company { get; set; }
+        public string ImageUrl { get; set; }
+        public string QuoteText { get; set; }
+    }
+}
