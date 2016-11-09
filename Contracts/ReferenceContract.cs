13ff3eeeabbcc0cc4ea87f712d25468a3e72d4ad
Rickard Kamel <rickardkamel@gmail.com>
2016-11-09 17:59:00 +0100

    Created ReferenceContract.

---
+++ Contracts/ReferenceContract.cs
@@ -0,0 +1,12 @@
+﻿namespace Contracts
+{
+    public class ReferenceContract
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Occupation { get; set; }
+        public string Company { get; set; }
+        public string ImageUrl { get; set; }
+        public string QuoteText { get; set; }
+    }
+}
