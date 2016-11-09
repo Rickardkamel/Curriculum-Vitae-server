e418f30c5e44004fab76d848eb66e79a408b8d5d
Rickard Kamel <rickardkamel@gmail.com>
2016-11-09 17:55:39 +0100

    Removed unused libraries.

--- BusinessLogic/Handlers/ProjectHandler.cs
+++ BusinessLogic/Handlers/ProjectHandler.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Contracts;
 using DataService;
 using DataService.UnitOfWork;
 using Mappers;
-using Models;
 
 namespace BusinessLogic.Handlers
 {
--- BusinessLogic/Handlers/ResumeHandler.cs
+++ BusinessLogic/Handlers/ResumeHandler.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Contracts;
 using DataService;
 using DataService.UnitOfWork;
 using Mappers;
-using Models;
 
 namespace BusinessLogic.Handlers
 {
