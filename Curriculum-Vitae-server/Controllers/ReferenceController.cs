6a856951595c13517cc58b1c0577a8f39d9e4909
Rickard Kamel <rickardkamel@gmail.com>
2016-11-09 17:59:10 +0100

    Created controller for Reference.

---
+++ Curriculum-Vitae-server/Controllers/ReferenceController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using BusinessLogic.Handlers;
+using Contracts;
+using DataService;
+
+namespace Curriculum_Vitae_server.Controllers
+{
+    public class ReferenceController : ApiController
+    {
+        private readonly ReferenceHandler _referenceHandler;
+
+        public ReferenceController()
+        {
+            _referenceHandler = new ReferenceHandler(new DataContext());
+        }
+
+        public IEnumerable<ReferenceContract> Get()
+        {
+            return _referenceHandler.Get();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var aboutMe = _referenceHandler.Get(id);
+            return aboutMe == null ? (IHttpActionResult)NotFound() : Ok(aboutMe);
+        }
+
+        public IHttpActionResult Post(ReferenceContract referenceContract)
+        {
+            _referenceHandler.Post(referenceContract);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            _referenceHandler.Delete(id);
+            return Ok();
+        }
+    }
+}
