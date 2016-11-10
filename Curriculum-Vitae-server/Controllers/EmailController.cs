67689f5656c4bca28e3db0bbb9120e705e04661d
Rickard Kamel <rickardkamel@gmail.com>
2016-11-11 00:08:35 +0100

    Added controller for Email.

---
+++ Curriculum-Vitae-server/Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusinessLogic.Handlers;
+using Contracts;
+using DataService;
+
+namespace Curriculum_Vitae_server.Controllers
+{
+    public class EmailController : ApiController
+    {
+        private readonly EmailHandler _emailHandler;
+
+        public EmailController()
+        {
+            _emailHandler = new EmailHandler(new DataContext());
+        }
+
+        public IHttpActionResult Post(EmailContract emailContract)
+        {
+            return _emailHandler.SendMail(emailContract) ? (IHttpActionResult) Ok() : BadRequest();
+        }
+    }
+}
