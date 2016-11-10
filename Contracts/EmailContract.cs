8ee0c35719097b57290b2e8588ef77a4ad1378bd
Rickard Kamel <rickardkamel@gmail.com>
2016-11-11 00:07:39 +0100

    Added contract for email.

---
+++ Contracts/EmailContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class EmailContract
+    {
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string From { get; set; }
+    }
+}
